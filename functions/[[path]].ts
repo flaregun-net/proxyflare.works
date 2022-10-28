d44c40060f19dd35f1aca5140fcba434413baed7
Network Chimp <support@networkchimp.com>
2022-10-27 22:06:30 -0500

    updated

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,9 +2,8 @@ import proxyflare from "@flaregun-net/proxyflare-for-pages"
 import { devConfig } from "../devConfig"
 
 const hosts = {
-  proxyflare: "proxyflare.works",
+  proxyflare: process.env.HOSTNAME,
   localhost: "http://localhost:8788",
 }
-console.log(`process.env.HOSTNAME`, process.env.HOSTNAME)
 
 export const onRequest = [proxyflare({ config: devConfig(hosts.proxyflare) })]
