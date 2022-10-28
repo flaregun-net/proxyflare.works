b1fa0387984dd95dd4d3fa3b0abf7cdfb3be9db2
Network Chimp <support@networkchimp.com>
2022-10-28 14:47:18 -0500

    updated

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,8 +1,10 @@
+import { devConfig } from "@flaregun-net/proxyflare-core"
 import proxyflare from "@flaregun-net/proxyflare-for-pages"
-import { devConfig } from "../devConfig"
 
 export const onRequest: PagesFunction<{ HOSTNAME: string }>[] = [
   (context) => {
+    // Here, devConfig isn't actually used in development because proxyflare-for-pages only runs in production (on proxyflare.works)
+    // In development, the proxyflare-core's local devConfig file is injected into a worker mounted on the dev site (proxyflare.xyz)
     return context.env.HOSTNAME === "proxyflare.works"
       ? proxyflare({ config: devConfig(context.env.HOSTNAME) })(context)
       : context.next()
