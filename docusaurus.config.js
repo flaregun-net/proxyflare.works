f9dec2071e5ad9f46b10cfd7338677fc3275d479
Network Chimp <hello@networkchimp.com>
2023-01-21 13:37:12 -0600

    updated discord invite link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
             "aria-label": "Flaregun on Github",
           },
           {
-            href: "https://discord.gg/YcDwujEH",
+            href: "https://discord.gg/ZqeZsA3qCT",
             position: "right",
             className: "header-discord-link",
             "aria-label": "Flaregun on Discord",
